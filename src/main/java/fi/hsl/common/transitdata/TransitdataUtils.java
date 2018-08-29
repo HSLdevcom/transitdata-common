1bde5f94559d87c850b013f277efc10c35286a2a
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-08-29 13:14:49 +0300

    Add util functions for reading env variables

--- src/main/java/fi/hsl/common/transitdata/TransitdataUtils.java
+++ src/main/java/fi/hsl/common/transitdata/TransitdataUtils.java
@@ -2,6 +2,7 @@ package fi.hsl.common.transitdata;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 public class TransitdataUtils {
     private TransitdataUtils() {}
@@ -16,4 +17,30 @@ public class TransitdataUtils {
         OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
         return utc.toEpochSecond();
     }
+
+
+    public static String getEnvOrThrow(String name) throws IllegalArgumentException {
+        return Optional.ofNullable(System.getenv(name))
+                .orElseThrow(() -> new IllegalArgumentException("Missing required env variable " + name));
+    }
+
+    public static Optional<String> getEnv(String name) {
+        return Optional.ofNullable(System.getenv(name));
+    }
+
+    public static Optional<Integer> safeParseInt(String value) {
+        try {
+            int n = Integer.parseInt(value);
+            return Optional.of(n);
+        }
+        catch (NumberFormatException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Integer> getIntEnv(String name) {
+        return Optional.ofNullable(System.getenv(name)).flatMap(TransitdataUtils::safeParseInt);
+    }
+
 }
