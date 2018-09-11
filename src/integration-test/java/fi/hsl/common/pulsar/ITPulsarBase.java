1c640ec2a102d3a176c19d693569228db9d47876
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-09-11 13:09:57 +0300

    Removed Junit5 and added integration tests setup

---
+++ src/integration-test/java/fi/hsl/common/pulsar/ITPulsarBase.java
@@ -0,0 +1,12 @@
+package fi.hsl.common.pulsar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ITPulsarBase {
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+}
---
+++ src/test/java/fi/hsl/common/config/ConfigParserTest.java
@@ -0,0 +1,12 @@
+package fi.hsl.common.config;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConfigParserTest {
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+}
