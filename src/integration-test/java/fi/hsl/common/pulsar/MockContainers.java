4a8290b86abd36cee6a03dc5e6757c258c0243a3
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-09-11 15:55:23 +0300

    Added first pulsar-app integration tests

---
+++ src/integration-test/java/fi/hsl/common/pulsar/MockContainers.java
@@ -0,0 +1,47 @@
+package fi.hsl.common.pulsar;
+
+import org.apache.pulsar.client.api.PulsarClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PulsarContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import redis.clients.jedis.Jedis;
+
+public class MockContainers {
+
+    static final Logger logger = LoggerFactory.getLogger(MockContainers.class);
+
+    private MockContainers() {}
+
+    public static GenericContainer newRedisContainer() {
+        return new GenericContainer("redis:3.0.2")
+                .withExposedPorts(6379);
+    }
+
+    public static Jedis newMockJedisConnection() {
+        return newMockJedisConnection(newRedisContainer());
+    }
+
+    public static Jedis newMockJedisConnection(GenericContainer redis) {
+        logger.info("Connecting to Redis container");
+        Jedis jedis = new Jedis(redis.getContainerIpAddress(), redis.getMappedPort(6379));
+        logger.info("Redis connected");
+        return jedis;
+    }
+
+    public static PulsarContainer newPulsarContainer() {
+        return new PulsarContainer("2.1.0-incubating");
+    }
+
+    public static PulsarClient newMockPulsarClient(PulsarContainer pulsar) throws Exception {
+        return PulsarClient.builder()
+                .serviceUrl(pulsar.getPulsarBrokerUrl())
+                .build();
+    }
+
+    public static void tail(GenericContainer container, Logger logger) {
+        Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
+        container.followOutput(logConsumer);
+    }
+}
---
+++ src/integration-test/java/fi/hsl/common/pulsar/PulsarMockApplication.java
@@ -0,0 +1,45 @@
+package fi.hsl.common.pulsar;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import fi.hsl.common.config.ConfigParser;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PulsarContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PulsarMockApplication extends PulsarApplication {
+
+    PulsarMockApplication() {
+    }
+
+    public static PulsarApplication newInstance(Config baseConfig, GenericContainer redis, PulsarContainer pulsar) throws Exception {
+        PulsarApplication app = null;
+        try {
+            Map<String, Object> overrides = new HashMap<>();
+            if (pulsar != null) {
+                overrides.put("pulsar.host", pulsar.getContainerIpAddress());
+                overrides.put("pulsar.port", pulsar.getMappedPort(PulsarContainer.PULSAR_PORT));
+            }
+            if (redis != null) {
+                overrides.put("redis.host", redis.getContainerIpAddress());
+            }
+
+            Config config = ConfigFactory.parseMap(overrides);
+
+            Config merged = ConfigParser.mergeConfigs(baseConfig, config);
+
+            app = new PulsarMockApplication();
+            app.context = app.initialize(merged);
+            return app;
+        }
+        catch (Exception e) {
+            if (app != null) {
+                app.close();
+            }
+            throw e;
+        }
+    }
+
+}
