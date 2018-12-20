85244bac96f2b0409f16f4b77509b8f94d93640c
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-12-20 13:53:31 +0200

    Added more mock generators

--- src/integration-test/java/fi/hsl/common/transitdata/MockDataUtils.java
+++ src/integration-test/java/fi/hsl/common/transitdata/MockDataUtils.java
@@ -2,8 +2,17 @@ package fi.hsl.common.transitdata;
 
 import nl.flotsam.xeger.Xeger;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class MockDataUtils {
     public static final String JORE_ROUTE_NAME_REGEX = "[0-9]{4}[A-Z]{1}[A-Z0-9]{0,1}";
+    public static final String ID_REGEX = "[0-9]{16}";
+
+    //16 chars of numbers
+    public static final long ID_MIN = 1000000000000000L;
+    public static final long ID_MAX = 9999999999999999L;
 
     private MockDataUtils() {}
 
@@ -11,4 +20,24 @@ public class MockDataUtils {
         Xeger generator = new Xeger(JORE_ROUTE_NAME_REGEX);
         return generator.generate();
     }
+
+    public static long generateValidJoreId() {
+        return generateLongWithMinAndMax(ID_MIN, ID_MAX);
+        //Xeger generator = new Xeger(DVJ_ID_REGEX);
+        //return Long.parseLong(generator.generate());
+    }
+
+    public static int generateStopSequenceId() {
+        return (int)generateLongWithMinAndMax(0, 50);
+    }
+
+    public static List<Integer> generateStopSequenceList(int length) {
+        return IntStream.range(0, length).boxed().collect(Collectors.toList());
+    }
+
+    private static long generateLongWithMinAndMax(long min, long max) {
+        long add = (long)(Math.random() * (max - min));
+        return min + add;
+    }
+
 }
--- src/integration-test/java/fi/hsl/common/transitdata/MockDataUtilsTest.java
+++ src/integration-test/java/fi/hsl/common/transitdata/MockDataUtilsTest.java
@@ -4,9 +4,14 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class MockDataUtilsTest {
@@ -19,5 +24,23 @@ public class MockDataUtilsTest {
         Matcher matcher = p.matcher(route);
         assertTrue(matcher.matches());
         logger.info(route);
+    }
+
+    @Test
+    public void testJoreIdGenerator() {
+        long id = MockDataUtils.generateValidJoreId();
+        assertTrue(id > 999999999999999L);
+        assertTrue(id < 10000000000000000L);
+        logger.info("id " + id);
+    }
+
+    @Test
+    public void testStopSeqListGenerator() {
+        final int length = 5;
+        List<Integer> seq = MockDataUtils.generateStopSequenceList(length);
+        assertEquals(length, seq.size());
+
+        Integer[] expected = {0, 1, 2, 3, 4};
+        assertTrue(Arrays.equals(expected, seq.toArray(new Integer[0])));
     }
 }
