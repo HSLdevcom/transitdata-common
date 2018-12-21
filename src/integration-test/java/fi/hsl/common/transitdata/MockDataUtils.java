30105d8cc07b61d90d8151b7af0110cdf91c4a87
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-12-21 07:11:44 +0200

    Common boilerplate to mock data

--- src/integration-test/java/fi/hsl/common/transitdata/MockDataUtils.java
+++ src/integration-test/java/fi/hsl/common/transitdata/MockDataUtils.java
@@ -1,5 +1,6 @@
 package fi.hsl.common.transitdata;
 
+import fi.hsl.common.transitdata.proto.PubtransTableProtos;
 import nl.flotsam.xeger.Xeger;
 
 import java.util.List;
@@ -8,7 +9,6 @@ import java.util.stream.IntStream;
 
 public class MockDataUtils {
     public static final String JORE_ROUTE_NAME_REGEX = "[0-9]{4}[A-Z]{1}[A-Z0-9]{0,1}";
-    public static final String ID_REGEX = "[0-9]{16}";
 
     //16 chars of numbers
     public static final long ID_MIN = 1000000000000000L;
@@ -23,11 +23,9 @@ public class MockDataUtils {
 
     public static long generateValidJoreId() {
         return generateLongWithMinAndMax(ID_MIN, ID_MAX);
-        //Xeger generator = new Xeger(DVJ_ID_REGEX);
-        //return Long.parseLong(generator.generate());
     }
 
-    public static int generateStopSequenceId() {
+    public static int generateValidStopSequenceId() {
         return (int)generateLongWithMinAndMax(0, 50);
     }
 
@@ -40,4 +38,65 @@ public class MockDataUtils {
         return min + add;
     }
 
+    public static long generateValidDateTimeMs() {
+        final long START_OF_2018 = 1514764800000L;
+        final long END_OF_2018 = 1546300799000L;
+
+        return generateLongWithMinAndMax(START_OF_2018, END_OF_2018);
+    }
+
+    public static PubtransTableProtos.Common.Builder generateValidCommon() {
+        return generateValidCommon(generateValidJoreId());
+    }
+
+    public static PubtransTableProtos.Common.Builder generateValidCommon(long dvjId) {
+        return generateValidCommon(dvjId, generateValidStopSequenceId());
+    }
+
+    public static PubtransTableProtos.Common.Builder generateValidCommon(long dvjId, int stopSequence) {
+        return generateValidCommon(dvjId, stopSequence, generateValidDateTimeMs());
+    }
+
+    public static PubtransTableProtos.Common.Builder generateValidCommon(long dvjId, int stopSequence, long targetDateTimeMs) {
+        return generateValidCommon(dvjId, stopSequence, targetDateTimeMs, System.currentTimeMillis());
+    }
+
+    public static PubtransTableProtos.Common.Builder generateValidCommon(long dvjId, int stopSequence, long targetDateTimeMs, long lastModifiedMs) {
+        return commonBoilerplate()
+                .setIsOnDatedVehicleJourneyId(dvjId)
+                .setJourneyPatternSequenceNumber(stopSequence)
+                .setTargetUtcDateTimeMs(targetDateTimeMs)
+                .setLastModifiedUtcDateTimeMs(lastModifiedMs);
+    }
+
+    public static PubtransTableProtos.Common.Builder commonBoilerplate() {
+        PubtransTableProtos.Common.Builder commonBuilder = PubtransTableProtos.Common.newBuilder();
+        commonBuilder.setIsTargetedAtJourneyPatternPointGid(generateValidJoreId());
+
+        commonBuilder.setSchemaVersion(commonBuilder.getSchemaVersion());
+
+        commonBuilder.setId(generateValidJoreId());
+        commonBuilder.setIsTimetabledAtJourneyPatternPointGid(1);
+        commonBuilder.setVisitCountNumber(2);
+        commonBuilder.setIsValidYesNo(true);
+
+        setIsViaPoint(commonBuilder, false);
+        setIsSkipped(commonBuilder, false);
+
+        return commonBuilder;
+    }
+
+    public static void setIsSkipped(PubtransTableProtos.Common.Builder builder, boolean skipped) {
+        if (skipped)
+            builder.setState(3L);
+        else
+            builder.setState(2L);
+    }
+
+    public static void setIsViaPoint(PubtransTableProtos.Common.Builder builder, boolean isViaPoint) {
+        if (isViaPoint)
+            builder.setType(0);
+        else
+            builder.setType(1);
+    }
 }
