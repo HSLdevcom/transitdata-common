877274cac79040fb846f248a01284c1baeefd35e
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2019-03-21 10:11:24 +0200

    Rename ThirtyHourTime to JoreDateTime, make JoreDateTime wrap both date and time, add utility methods to JoreDateTime, update JoreDateTimeTest

--- src/main/java/fi/hsl/common/transitdata/TransitdataProperties.java
+++ src/main/java/fi/hsl/common/transitdata/TransitdataProperties.java
@@ -1,6 +1,6 @@
 package fi.hsl.common.transitdata;
 
-import fi.hsl.common.gtfsrt.ThirtyHourTime;
+import fi.hsl.common.gtfsrt.JoreDateTime;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
@@ -106,7 +106,7 @@ public class TransitdataProperties {
         return REDIS_PREFIX_JORE_ID + route + "-" + direction + "-" + startDate + "-" + startTime;
     }
 
-    public static String formatJoreId(String route, String direction, String startDate, ThirtyHourTime startTime) {
-        return formatJoreId(route, direction, startDate, startTime.getTimeString());
+    public static String formatJoreId(String route, String direction, JoreDateTime startDateTime) {
+        return formatJoreId(route, direction, startDateTime.getJoreDateString(), startDateTime.getJoreTimeString());
     }
 }
