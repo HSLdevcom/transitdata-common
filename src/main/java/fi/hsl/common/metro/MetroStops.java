7cdb5e3c23861321bd0b3c5e82ca6d73f2c2f93d
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2019-04-04 09:01:53 +0300

    Add methods getJoreDirection and getStopNumber to MetroStops, change return types to Optional, add corresponding tests

--- src/main/java/fi/hsl/common/metro/MetroStops.java
+++ src/main/java/fi/hsl/common/metro/MetroStops.java
@@ -4,12 +4,15 @@ import com.typesafe.config.Config;
 import fi.hsl.common.config.ConfigParser;
 import org.apache.pulsar.shade.com.google.common.collect.ArrayListMultimap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 public class MetroStops {
     private static final HashMap<String, String> shortNameByStopNumber = new HashMap<>();
     private static final ArrayListMultimap<String, String> stopNumbersByShortName = ArrayListMultimap.create();
+    private static final List<String> shortNames = new ArrayList<>();
 
     static {
         final Config stopsConfig = ConfigParser.createConfig("metro_stops.conf");
@@ -18,6 +21,7 @@ public class MetroStops {
                     final Config stopConfig = stopConfigObject.toConfig();
                     final String shortName = stopConfig.getString("shortName");
                     final List<String> stopNumbers = stopConfig.getStringList("stopNumbers");
+                    shortNames.add(shortName);
                     stopNumbers.forEach(stopNumber -> {
                         shortNameByStopNumber.put(stopNumber, shortName);
                         stopNumbersByShortName.put(shortName, stopNumber);
@@ -27,11 +31,41 @@ public class MetroStops {
 
     private MetroStops() {}
 
-    public static String getShortName(final String stopNumber) {
-        return shortNameByStopNumber.get(stopNumber);
+    public static Optional<String> getShortName(final String stopNumber) {
+        return Optional.ofNullable(shortNameByStopNumber.get(stopNumber));
     }
 
-    public static List<String> getStopNumbers(final String shortName) {
-        return stopNumbersByShortName.get(shortName);
+    public static Optional<List<String>> getStopNumbers(final String shortName) {
+        List<String> stopNumbers = stopNumbersByShortName.get(shortName);
+        return stopNumbers.size() > 0 ? Optional.ofNullable(stopNumbers) : Optional.empty();
+    }
+
+    public static Optional<Integer> getJoreDirection(final String startStop, final String endStop) {
+        final int startStopIndex = shortNames.indexOf(startStop);
+        final int endStopIndex = shortNames.indexOf(endStop);
+        if (startStopIndex == -1 || endStopIndex == -1 || startStopIndex == endStopIndex) {
+            return Optional.empty();
+        }
+        return Optional.of(startStopIndex < endStopIndex ? 1 : 2);
+    }
+
+    public static Optional<String> getStopNumber(final String shortName, final int joreDirection) {
+        Optional<List<String>> stopNumbers = getStopNumbers(shortName);
+        if (joreDirection < 1 || joreDirection > 2 || !stopNumbers.isPresent()) {
+            return Optional.empty();
+        }
+        // The first stop number corresponds Jore direction 1 and the second stop number corresponds Jore direction 2
+        String stopNumber;
+        try {
+            stopNumber = stopNumbers.get().get(joreDirection - 1);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(stopNumber);
+    }
+
+    public static Optional<String> getStopNumber(final String shortName, final String startStop, final String endStop) {
+        final Optional<Integer> joreDirection = getJoreDirection(startStop, endStop);
+        return joreDirection.isPresent() ? getStopNumber(shortName, joreDirection.get()) : Optional.empty();
     }
 }
