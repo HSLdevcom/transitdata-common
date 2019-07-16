2c34114b9f7b137d53e3cb5b6a557965ab84925b
Joe Niemi <niemijoe@hotmail.com>
2019-07-16 16:24:07 +0300

    Release/1.2.1 (#113)
    * Add metro_stops.conf, add MetroStops singleton class, add MetroStopsTest
    
    * Remove unnecessary code from MetroStops
    
    * Add Redis key names for metro data to TransitdataProperties
    
    * Add method formatMetroId to TransitdataProperties
    
    * Add field last_modified_utc_date_time_ms to mqtt.proto
    
    * Add methods getJoreDirection and getStopNumber to MetroStops, change return types to Optional, add corresponding tests
    
    * Revert "Add field last_modified_utc_date_time_ms to mqtt.proto"
    
    This reverts commit 50c61329c879f7b1515b5ed7636776c2f43cb7e6.
    
    * Use PubtransFactory.JORE_DIRECTION_ID_OUTBOUND and PubtransFactory.JORE_DIRECTION_ID_INBOUND in MetroStops
    
    * Update version to 0.8.4
    
    * Change getStopNumbers() to return List instead of Optional, change tests accordingly
    
    * Added metro-ats.proto, fix to formatMetroId method
    
    * metro_row -> metro_rows, fixed Message field declaration order, some typo fixes
    
    * Reverting common version
    
    * Removed MetroStops
    
    * refactored: start_stop_short_name comes from mqtt instead of redis
    
    * Escaped dot
    
    * removed start-stop-short-name property
    
    * Release version 1.2.1

--- src/main/java/fi/hsl/common/transitdata/TransitdataProperties.java
+++ src/main/java/fi/hsl/common/transitdata/TransitdataProperties.java
@@ -1,17 +1,24 @@
 package fi.hsl.common.transitdata;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Keys and corresponding values that are shared in the Transitdata pipeline.
  */
 public class TransitdataProperties {
     private TransitdataProperties() {}
+    private static final Pattern matchTimestampSecondsAndMillisecondsPattern = Pattern.compile("\\d{2}\\.\\d{3}Z$");
 
     public static final String REDIS_PREFIX_JPP = "jpp:";
     public static final String REDIS_PREFIX_DVJ = "dvj:";
     public static final String REDIS_PREFIX_JORE_ID = "jore:";
+    public static final String REDIS_PREFIX_METRO = "metro:";
 
     public static final String KEY_PROTOBUF_SCHEMA = "protobuf-schema";
     public static final String KEY_SCHEMA_VERSION = "schema-version";
@@ -23,6 +30,8 @@ public class TransitdataProperties {
     public static final String KEY_OPERATING_DAY = "operating-day";
     public static final String KEY_STOP_ID = "stop-id";
     public static final String KEY_LAST_CACHE_UPDATE_TIMESTAMP = "cache-update-ts";
+    public static final String KEY_START_DATETIME = "start-datetime";
+    public static final String KEY_START_STOP_NUMBER = "start-stop-number";
 
     /**
      * Describes the payload format for each message so that the data can be de-serialized.
@@ -37,6 +46,7 @@ public class TransitdataProperties {
         GTFS_VehiclePosition,
         InternalMessagesTripCancellation,
         InternalMessagesStopEstimate,
+        MetroAtsEstimate,
         MqttRawMessage,
         HfpData,
         TransitdataServiceAlert;
@@ -50,6 +60,7 @@ public class TransitdataProperties {
                 case GTFS_VehiclePosition: return "gtfs-vehicle-position";
                 case InternalMessagesTripCancellation: return "internal-messages-trip-cancellation";
                 case InternalMessagesStopEstimate: return "internal-messages-stop-estimate";
+                case MetroAtsEstimate: return "metro-ats-estimate";
                 case MqttRawMessage: return "mqtt-raw";
                 case HfpData: return "hfp-data";
                 case TransitdataServiceAlert: return "transitdata-service-alert";
@@ -78,6 +89,9 @@ public class TransitdataProperties {
             }
             else if (str.equals(InternalMessagesStopEstimate.toString())) {
                 return InternalMessagesStopEstimate;
+            }
+            else if (str.equals(MetroAtsEstimate.toString())) {
+                return MetroAtsEstimate;
             }
             else if (str.equals(MqttRawMessage.toString())) {
                 return MqttRawMessage;
@@ -112,4 +126,19 @@ public class TransitdataProperties {
     public static String formatJoreId(String route, String direction, JoreDateTime startDateTime) {
         return formatJoreId(route, direction, startDateTime.getJoreDateString(), startDateTime.getJoreTimeString());
     }
+
+    /**
+     * @param stopShortName e.g. MAK
+     * @param originalStartDatetime e.g. 2019-12-20T15:12:56.123Z
+     * @return
+     */
+    public static String formatMetroId(final String stopShortName, final String originalStartDatetime) {
+        // Transform dateTime string: remove milliseconds, change seconds
+        // e.g 2019-12-20T15:12:56.123Z --> 2019-12-20T15:12:00Z
+        Matcher m = matchTimestampSecondsAndMillisecondsPattern.matcher(originalStartDatetime);
+        String fixedStartDateTime = m.replaceFirst("00Z");
+
+        return REDIS_PREFIX_METRO + stopShortName + "_" + fixedStartDateTime;
+    }
+
 }
