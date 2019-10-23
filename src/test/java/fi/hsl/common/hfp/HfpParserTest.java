b5423d1c806ffe4d724ef44f73beb200dad58110
Joose Helle <joose.helle@gmail.com>
2019-10-23 14:32:11 +0300

    Update HfpParser tests with added (tlr) properties

--- src/test/java/fi/hsl/common/hfp/HfpParserTest.java
+++ src/test/java/fi/hsl/common/hfp/HfpParserTest.java
@@ -47,6 +47,11 @@ public class HfpParserTest {
         assertTrue(636 == hfp.payload.jrn);
         assertTrue(112 == hfp.payload.line);
         assertEquals("20:25", hfp.payload.start);
+        assertTrue(10 == hfp.payload.occu);
+        assertTrue(1 == hfp.payload.seq);
+        assertEquals("2018-04-05T17:38:36Z", hfp.payload.ttarr);
+        assertEquals("2018-04-05T17:38:36Z", hfp.payload.ttdep);
+        assertTrue(0 == hfp.payload.dr_type);
     }
 
     @Test
@@ -77,6 +82,98 @@ public class HfpParserTest {
     }
 
     @Test
+    public void parseSampleTlrJsonFile() throws Exception {
+        HfpJson hfp = parseJsonFromResources("hfp-tlr-sample.json");
+        assertEquals("112", hfp.payload.desi);
+        assertEquals("1", hfp.payload.dir);
+        assertTrue(22 == hfp.payload.oper);
+        assertTrue(754 == hfp.payload.veh);
+        assertEquals("2019-10-21T07:16:36.992Z", hfp.payload.tst);
+        assertTrue(1571642196 == hfp.payload.tsi);
+        assertTrue(11.51 - hfp.payload.spd < 0.00001f);
+        assertTrue(241 == hfp.payload.hdg);
+        assertTrue(60.174129 - hfp.payload.lat < 0.00001f);
+        assertTrue(24.796323 - hfp.payload.longitude < 0.00001f);
+        assertTrue(0.58 == hfp.payload.acc);
+        assertTrue(-28 == hfp.payload.dl);
+        assertTrue(653 == hfp.payload.odo);
+        assertTrue(0 == hfp.payload.drst);
+        assertEquals("2019-10-21", hfp.payload.oday);
+        assertTrue(102 == hfp.payload.jrn);
+        assertTrue(227 == hfp.payload.line);
+        assertEquals("10:15", hfp.payload.start);
+        assertEquals("GPS", hfp.payload.loc);
+        assertEquals(null, hfp.payload.stop);
+        assertEquals("2112", hfp.payload.route);
+        assertTrue(0 == hfp.payload.occu);
+        assertTrue(2107 == hfp.payload.sid);
+        assertTrue(894 == hfp.payload.signal_groupid);
+        assertTrue(1 == hfp.payload.tlp_signalgroupnbr);
+        assertEquals("NORMAL", hfp.payload.tlp_requesttype);
+        assertTrue(100 == hfp.payload.tlp_requestid);
+        assertEquals("normal", hfp.payload.tlp_prioritylevel);
+        assertEquals(null, hfp.payload.tlp_line_configid);
+        assertEquals(null, hfp.payload.tlp_point_configid);
+        assertTrue(461200 == hfp.payload.tlp_frequency);
+        assertEquals("KAR", hfp.payload.tlp_protocol);
+        assertTrue(1 == hfp.payload.tlp_att_seq);
+        assertEquals("AHEAD", hfp.payload.tlp_reason);
+    }
+
+    @Test
+    public void parseValidTlrJsonFileToProtobuf() throws Exception {
+        HfpJson json = parseJsonFromResources("hfp-tlr-sample.json");
+        Hfp.Payload hfp = HfpParser.parsePayload(json);
+        assertEquals("10:15", hfp.getStart());
+        assertTrue(true == hfp.hasLoc());
+        assertTrue(Hfp.Payload.LocationQualityMethod.GPS == hfp.getLoc());
+        assertTrue(0 == hfp.getStop());
+        assertEquals("2112", hfp.getRoute());
+        assertTrue(0 == hfp.getOccu());
+        assertTrue(2107 == hfp.getSid());
+        assertTrue(894 == hfp.getSignalGroupid());
+        assertTrue(1 == hfp.getTlpSignalgroupnbr());
+        assertTrue(true == hfp.hasTlpRequesttype());
+        assertEquals(Hfp.Payload.TlpRequestType.NORMAL, hfp.getTlpRequesttype());
+        assertTrue(100 == hfp.getTlpRequestid());
+        assertTrue(true == hfp.hasTlpPrioritylevel());
+        assertEquals(Hfp.Payload.TlpPriorityLevel.normal, hfp.getTlpPrioritylevel());
+        assertEquals(false, hfp.hasTlpLineConfigid());
+        assertEquals(false, hfp.hasTlpPointConfigid());
+        assertTrue(461200 == hfp.getTlpFrequency());
+        assertEquals("KAR", hfp.getTlpProtocol());
+        assertTrue(1 == hfp.getTlpAttSeq());
+        assertTrue(true == hfp.hasTlpReason());
+        assertEquals(Hfp.Payload.TlpReason.AHEAD, hfp.getTlpReason());
+        assertTrue(true == hfp.hasTlpDecision());
+        assertEquals(Hfp.Payload.TlpDecision.NAK, hfp.getTlpDecision());
+    }
+
+    @Test
+    public void parseInvalidTlrFieldsJsonFileToProtobuf() throws Exception {
+        HfpJson json = parseJsonFromResources("hfp-tlr-sample-invalid.json");
+        Hfp.Payload hfp = HfpParser.parsePayload(json);
+        assertEquals(false, hfp.hasLoc());
+        assertEquals(false, hfp.hasTlpRequesttype());
+        assertEquals(false, hfp.hasTlpPrioritylevel());
+        assertEquals(false, hfp.hasTlpProtocol());
+        assertEquals(false, hfp.hasTlpReason());
+        assertEquals(false, hfp.hasTlpDecision());
+    }
+
+     @Test
+    public void parseMissingTlrFiledsJsonFileToProtobuf() throws Exception {
+        HfpJson json = parseJsonFromResources("hfp-tlr-sample-missing.json");
+        Hfp.Payload hfp = HfpParser.parsePayload(json);
+        assertEquals(false, hfp.hasLoc());
+        assertEquals(false, hfp.hasTlpRequesttype());
+        assertEquals(false, hfp.hasTlpPrioritylevel());
+        assertEquals(false, hfp.hasTlpProtocol());
+        assertEquals(false, hfp.hasTlpReason());
+        assertEquals(false, hfp.hasTlpDecision());
+    }
+
+    @Test
     public void parseSampleJsonFileToProtobuf() throws Exception {
         HfpJson json = parseJsonFromResources("hfp-sample-vp.json");
         Hfp.Payload hfp = HfpParser.parsePayload(json);
@@ -99,6 +196,11 @@ public class HfpParserTest {
         assertTrue(636 == hfp.getJrn());
         assertTrue(112 == hfp.getLine());
         assertEquals("20:25", hfp.getStart());
+        assertTrue(10 == hfp.getOccu());
+        assertTrue(1 == hfp.getSeq());
+        assertEquals("2018-04-05T17:38:36Z", hfp.getTtarr());
+        assertEquals("2018-04-05T17:38:36Z", hfp.getTtdep());
+        assertTrue(0 == hfp.getDrType());
     }
 
     @Test
