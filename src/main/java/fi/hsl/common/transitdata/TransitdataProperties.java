66b827cc9bf69a09743b492180d61bf5d4c00158
Aki Väliaho <aki.valiaho@hsl.fi>
2020-05-12 20:53:51 +0300

    Register EKE proto as possible alternative

--- src/main/java/fi/hsl/common/transitdata/TransitdataProperties.java
+++ src/main/java/fi/hsl/common/transitdata/TransitdataProperties.java
@@ -1,29 +1,19 @@
 package fi.hsl.common.transitdata;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.time.*;
+import java.util.regex.*;
 
 /**
  * Keys and corresponding values that are shared in the Transitdata pipeline.
  */
 public class TransitdataProperties {
-    private TransitdataProperties() {}
-    private static final Pattern matchTimestampSecondsAndMillisecondsPattern = Pattern.compile("\\d{2}\\.\\d{3}Z$");
-
     public static final String REDIS_PREFIX_JPP = "jpp:";
     public static final String REDIS_PREFIX_DVJ = "dvj:";
     public static final String REDIS_PREFIX_JORE_ID = "jore:";
     public static final String REDIS_PREFIX_METRO = "metro:";
-
     public static final String KEY_PROTOBUF_SCHEMA = "protobuf-schema";
     public static final String KEY_SCHEMA_VERSION = "schema-version";
     public static final String KEY_DVJ_ID = "dvj-id";
-
     public static final String KEY_ROUTE_NAME = "route-name";
     public static final String KEY_DIRECTION = "direction";
     public static final String KEY_START_TIME = "start-time";
@@ -32,6 +22,43 @@ public class TransitdataProperties {
     public static final String KEY_LAST_CACHE_UPDATE_TIMESTAMP = "cache-update-ts";
     public static final String KEY_START_DATETIME = "start-datetime";
     public static final String KEY_START_STOP_NUMBER = "start-stop-number";
+    private static final Pattern matchTimestampSecondsAndMillisecondsPattern = Pattern.compile("\\d{2}\\.\\d{3}Z$");
+
+    private TransitdataProperties() {
+    }
+
+    /**
+     * GTFS-RT convention is to use UTC epoch seconds so let's use the same convention.
+     *
+     * @return UTC epoch seconds
+     * @See https://developers.google.com/transit/gtfs-realtime/reference/#message_feedheader
+     */
+    public static long currentTimestamp() {
+        OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
+        return utc.toEpochSecond();
+    }
+
+    public static String formatJoreId(String route, String direction, JoreDateTime startDateTime) {
+        return formatJoreId(route, direction, startDateTime.getJoreDateString(), startDateTime.getJoreTimeString());
+    }
+
+    public static String formatJoreId(String route, String direction, String startDate, String startTime) {
+        return REDIS_PREFIX_JORE_ID + route + "-" + direction + "-" + startDate + "-" + startTime;
+    }
+
+    /**
+     * @param stopShortName         e.g. MAK
+     * @param originalStartDatetime e.g. 2019-12-20T15:12:56.123Z
+     * @return
+     */
+    public static String formatMetroId(final String stopShortName, final String originalStartDatetime) {
+        // Transform dateTime string: remove milliseconds, change seconds
+        // e.g 2019-12-20T15:12:56.123Z --> 2019-12-20T15:12:00Z
+        Matcher m = matchTimestampSecondsAndMillisecondsPattern.matcher(originalStartDatetime);
+        String fixedStartDateTime = m.replaceFirst("00Z");
+
+        return REDIS_PREFIX_METRO + stopShortName + "_" + fixedStartDateTime;
+    }
 
     /**
      * Describes the payload format for each message so that the data can be de-serialized.
@@ -46,19 +73,16 @@ public class TransitdataProperties {
         GTFS_VehiclePosition("gtfs-vehicle-position"),
         InternalMessagesTripCancellation("internal-messages-trip-cancellation"),
         InternalMessagesStopEstimate("internal-messages-stop-estimate"),
-        MetroAtsEstimate( "metro-ats-estimate"),
+        MetroAtsEstimate("metro-ats-estimate"),
         MqttRawMessage("mqtt-raw"),
         HfpData("hfp-data"),
         TransitdataServiceAlert("transitdata-service-alert"),
-        StopCancellations("stop-cancellations");
+        StopCancellations("stop-cancellations"),
+        FiHslEke("FiHslEke");
         public final String key;
 
         ProtobufSchema(String key) {
             this.key = key;
-        }
-
-        public String toString() {
-            return key;
         }
 
         public static ProtobufSchema fromString(String str) {
@@ -68,41 +92,12 @@ public class TransitdataProperties {
                 }
             }
 
-            throw new IllegalArgumentException("No protobuf schema found for string "+str);
+            throw new IllegalArgumentException("No protobuf schema found for string " + str);
         }
-    }
 
-    /**
-     * GTFS-RT convention is to use UTC epoch seconds so let's use the same convention.
-     * @See https://developers.google.com/transit/gtfs-realtime/reference/#message_feedheader
-     *
-     * @return UTC epoch seconds
-     */
-    public static long currentTimestamp() {
-        OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
-        return utc.toEpochSecond();
-    }
-
-    public static String formatJoreId(String route, String direction, String startDate, String startTime) {
-        return REDIS_PREFIX_JORE_ID + route + "-" + direction + "-" + startDate + "-" + startTime;
-    }
-
-    public static String formatJoreId(String route, String direction, JoreDateTime startDateTime) {
-        return formatJoreId(route, direction, startDateTime.getJoreDateString(), startDateTime.getJoreTimeString());
-    }
-
-    /**
-     * @param stopShortName e.g. MAK
-     * @param originalStartDatetime e.g. 2019-12-20T15:12:56.123Z
-     * @return
-     */
-    public static String formatMetroId(final String stopShortName, final String originalStartDatetime) {
-        // Transform dateTime string: remove milliseconds, change seconds
-        // e.g 2019-12-20T15:12:56.123Z --> 2019-12-20T15:12:00Z
-        Matcher m = matchTimestampSecondsAndMillisecondsPattern.matcher(originalStartDatetime);
-        String fixedStartDateTime = m.replaceFirst("00Z");
-
-        return REDIS_PREFIX_METRO + stopShortName + "_" + fixedStartDateTime;
+        public String toString() {
+            return key;
+        }
     }
 
 }
