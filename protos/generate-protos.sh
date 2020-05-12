1c6932f8aef9efeae4c06e451986644ee5a8c4b6
Aki Väliaho <aki.valiaho@hsl.fi>
2020-05-12 20:49:58 +0300

    Include EKE proto spec

--- protos/generate-protos.sh
+++ protos/generate-protos.sh
@@ -13,3 +13,5 @@ protoc mqtt.proto --java_out=../src/main/java/
 protoc hfp.proto --java_out=../src/main/java/
 
 protoc metro-ats.proto --java_out=../src/main/java/
+
+protoc fi_hsl_eke.proto --java_out=../src/main/java
